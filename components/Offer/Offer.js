556992033bf113764f17da86d62ff3e87f5cb960
tenzzskit <tenzzskiy@mail.ru>
2022-01-21 16:36:18 +0300

    7 day - made Preferences

--- components/Offer/Offer.js
+++ components/Offer/Offer.js
@@ -1,4 +1,4 @@
-import React, {FunctionComponent} from "react";
+import React from "react";
 import styles from './Offer.module.scss'
 import {OfferCard} from "./OfferCard/OfferCard";
 import {useState} from "react";
@@ -8,7 +8,7 @@ import data from './../Shop/cart_arenda-plasm77.ru.json'
 
 
 
-export const Offer:FunctionComponent  = ({ }) =>{
+export const Offer = ({ }) =>{
     // const [currentSlide, setCurrentSlide] = React.useState(0);
     // const [loaded, setLoaded] = useState(false);
     // const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
@@ -119,14 +119,14 @@ export const Offer:FunctionComponent  = ({ }) =>{
                         <>
                             <Arrow
                                 left
-                                onClick={(e: any) =>
+                                onClick={(e) =>
                                     e.stopPropagation() || instanceRef.current?.prev()
                                 }
                                 disabled={currentSlide === 0}
                             />
 
                             <Arrow
-                                onClick={(e: any) =>
+                                onClick={(e) =>
                                     e.stopPropagation() || instanceRef.current?.next()
                                 }
                                 disabled={
@@ -192,28 +192,28 @@ export const Offer:FunctionComponent  = ({ }) =>{
 //     );
 // }
 
-function Arrow(props: {
-    disabled: boolean;
-    left?: boolean;
-    onClick: (e: any) => void;
+function Arrow({
+    disabled,
+    left,
+    onClick
 }) {
-    const disabeld = props.disabled ? "arrow--disabled" : "";
+    const disabeld = disabled ? "arrow--disabled" : "";
     return (
         <div className={`button-block ${
-            props.left ? "button-block--left" : "button-block--right"
+            left ? "button-block--left" : "button-block--right"
         } ${disabeld}`} >
         <svg
-            onClick={props.onClick}
+            onClick={onClick}
             className={`arrow ${
-                props.left ? "arrow--left" : "arrow--right"
+                left ? "arrow--left" : "arrow--right"
             } `}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
         >
-            {props.left && (
+            {left && (
                 <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
             )}
-            {!props.left && (
+            {!left && (
                 <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
             )}
         </svg>
